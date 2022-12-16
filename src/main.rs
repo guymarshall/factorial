6aff3a05f1ea95a0a14b3d117f537cd993cb22c6
Guy Marshall <guymarshall9@gmail.com>
2022-12-16 23:54:39 +0000

    Get bigint working with example code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+extern crate num;
+use num::BigInt;
+use std::ops::Mul;
+
 fn main() {
-    println!("Hello, world!");
+    let x = BigInt::parse_bytes(b"12345678901234567890", 10).unwrap();
+    let y = BigInt::parse_bytes(b"9876543210987654321", 10).unwrap();
+
+    let result = x.mul(y);
+
+    println!("{}", result);  // Output: 12193263111263526400
 }
