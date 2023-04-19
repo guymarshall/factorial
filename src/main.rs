9b971de9cacae43d12d2f43cb28ca81220bc1b87
Guy Marshall <guymarshall9@gmail.com>
2023-04-19 20:13:12 +0100

    Add if statements for number checking to main function

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,14 @@ mod factorial;
 fn main() {
     let user_input: i32 = user_input::get_user_input("Enter number: ");
 
+    if user_input < 0 {
+        panic!("Factorial is not defined for negative numbers");
+    }
+
+    if user_input == 0 {
+        println!("Factorial of 0 is 1");
+        return;
+    }
+
     println!("{}! = {}", user_input, factorial::factorial(user_input));
 }
