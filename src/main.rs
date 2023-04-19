87835ca31d1a85554216b163aefc01c33cfe4958
Guy Marshall <guymarshall9@gmail.com>
2023-04-19 20:21:00 +0100

    Use new input function

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod user_input;
 mod factorial;
 
 fn main() {
-    let user_input: i32 = user_input::get_user_input("Enter number: ");
+    let user_input: i32 = user_input::input("Enter number: ");
 
     if user_input < 0 {
         println!("Factorial is not defined for negative numbers");
