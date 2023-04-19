8830b61c3ecadffa125223ec253c5d4353215fee
Guy Marshall <guymarshall9@gmail.com>
2023-04-19 21:16:12 +0100

    Add format_scientific

--- src/factorial.rs
+++ src/factorial.rs
@@ -11,3 +11,10 @@ fn number_to_vector(number: i32) -> Vec<BigInt> {
 pub fn factorial(number: i32) -> BigInt {
     number_to_vector(number).iter().fold(BigInt::from(1), |acc, x| acc * x)
 }
+
+pub fn format_scientific(number: &BigInt) -> String {
+    let digit_count: i64 = number.to_string().len() as i64;
+    let exponent: i64 = digit_count - 1;
+    let mantissa: BigInt = number / BigInt::from(10).pow(exponent as u32);
+    format!("{}x10^{}", mantissa.to_string(), exponent)
+}
