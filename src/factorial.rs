535cc7677adb4b09c7dd6f1aae382e1d7d8d17c4
Guy Marshall <guymarshall9@gmail.com>
2023-12-18 10:46:02 +0000

    UPDATE: Remove unnecessary closure, to_string() AND give variables better names

--- src/factorial.rs
+++ src/factorial.rs
@@ -5,16 +5,16 @@ use num::BigInt;
 use rayon::prelude::*;
 
 fn number_to_vector(number: i32) -> Vec<BigInt> {
-    (1..=number).into_par_iter().map(|i| BigInt::from(i)).collect()
+    (1..=number).into_par_iter().map(BigInt::from).collect()
 }
 
 pub fn factorial(number: i32) -> BigInt {
-    number_to_vector(number).iter().fold(BigInt::from(1), |acc, x| acc * x)
+    number_to_vector(number).iter().fold(BigInt::from(1), |accumulator, current| accumulator * current)
 }
 
 pub fn format_scientific(number: &BigInt) -> String {
     let digit_count: i64 = number.to_string().len() as i64;
     let exponent: i64 = digit_count - 1;
     let mantissa: BigInt = number / BigInt::from(10).pow(exponent as u32);
-    format!("{}x10^{}", mantissa.to_string(), exponent)
+    format!("{}x10^{}", mantissa, exponent)
 }
