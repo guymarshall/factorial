1eeea929e615c2a0b1b72e275ed190e74fbdcfeb
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2024-06-01 17:00:59 +0100

    UPDATE: Print factorial in scientific notation to prevent excessive output

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,20 @@
+import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
 import java.util.Scanner;
-import java.util.stream.LongStream;
+import java.util.stream.IntStream;
 
 public class Main {
+    public static String formatScientific(BigInteger number) {
+        BigDecimal numberDecimal = new BigDecimal(number);
+        DecimalFormat decimalFormat = new DecimalFormat("0.00E0", DecimalFormatSymbols.getInstance(Locale.ROOT));
+        return decimalFormat.format(numberDecimal);
+    }
+
     public static BigInteger factorial(int number) {
-        return LongStream.rangeClosed(2, number)
+        return IntStream.rangeClosed(2, number)
                 .parallel()
                 .mapToObj(BigInteger::valueOf)
                 .reduce(BigInteger.ONE, BigInteger::multiply);
@@ -17,7 +27,8 @@ public class Main {
 
         long startTime = System.nanoTime();
         BigInteger factorial = factorial(number);
-        System.out.printf("%d! = %d%n", number, factorial);
+        String factorialFormatted = formatScientific(factorial);
+        System.out.printf("%d! = %s%n", number, factorialFormatted);
 
         long endTime = System.nanoTime();
         System.out.printf("Time: %dms%n", (endTime - startTime) / 1000000);
