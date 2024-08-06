330b1f452120cbedc28e951bd6e078836a6cc1ce
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2024-08-06 17:18:35 +0100

    UPDATE: Convert to much faster Rust implementation

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+mod user_input;
+
+extern crate num;
+
+use std::time::Instant;
+
+use bigdecimal::BigDecimal;
+use num::BigInt;
+use rayon::prelude::*;
+
+fn format_scientific(number: BigInt) -> String {
+    if number < BigInt::from(1000) {
+        return number.to_string();
+    }
+
+    let number_decimal: BigDecimal = BigDecimal::from(number);
+    format!("{:.2E}", number_decimal)
+}
+
+fn calculate_factorial(number: i32) -> BigInt {
+    (2..=number)
+        .into_par_iter()
+        .map(BigInt::from)
+        .reduce_with(|a, b| a * b)
+        .unwrap_or(BigInt::from(1))
+}
+
+fn main() {
+    let number: i32 = user_input::input("Enter number:");
+
+    let start_time: Instant = Instant::now();
+    let factorial: BigInt = calculate_factorial(number);
+
+    println!("Formatting...");
+    let factorial_formatted: String = format_scientific(factorial);
+    println!("{}! = {}", number, factorial_formatted);
+
+    let end_time: Instant = Instant::now();
+    println!("Elapsed time: {:?}", end_time - start_time);
+}
